ad8a7fb6078ee52285f4b4006488f6199fc2228a
nikita-nafikov <nafikov.nik@mail.ru>
2024-03-04 12:20:46 +0500

    fix route path

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,9 +3,9 @@ import FormPage from "../pages/FormPage.vue";
 import ComplitedForm from "../pages/ComplitedForm.vue";
 
 const routes = [
-  { path: "/", component: MainPage },
-  { path: "/form-create", component: FormPage },
-  { path: "/complited-from", component: ComplitedForm },
+  { path: "/security-audit/", component: MainPage },
+  { path: "/security-audit/form-create", component: FormPage },
+  { path: "/security-audit/complited-from", component: ComplitedForm },
 ];
 
 export default routes;
